12564f7df1f324672d2c9eb04e7bc8ab12b61844
Pierre-Louis Grangier <pierrelouis.grangier@gmail.com>
2024-10-24 14:24:14 +0200

    ThymeLead parcours des utilisateurs dans la page keyce

--- src/main/java/com/keyce/demo/controller/PageController.java
+++ src/main/java/com/keyce/demo/controller/PageController.java
@@ -1,23 +1,30 @@
 package com.keyce.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.keyce.demo.service.UserService;
+
 @Controller
 public class PageController {
 
-    /** Home page.*/
-    @GetMapping("/")
-    public String index(Model model) {
-    	model.addAttribute("message","Welcome");
-        return "index";
-    }
-    
-    /** Home page.*/
-    @GetMapping("/keyce")
-    public String keyce(Model model) {
-    	model.addAttribute("message","Welcome to Keyce");
-        return "keyce/index";
-    }
+	@Autowired
+	private UserService userService;
+
+	/** Home page. */
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("message", "Welcome");
+		return "home";
+	}
+
+	/** Home page. */
+	@GetMapping("/keyce")
+	public String keyce(Model model) {
+		model.addAttribute("message", "Welcome to Keyce");
+		model.addAttribute("users", userService.getAllUsers());
+		return "keyce";
+	}
 }
